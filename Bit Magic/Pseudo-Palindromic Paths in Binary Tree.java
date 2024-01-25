0032eebb2f6983922450416edee875bcd110099e
Abhishek Bisht <dabhishekbisht@gmail.com>
2024-01-25 23:35:03 +0530

    Added Big Magic folder

---
+++ Bit Magic/Pseudo-Palindromic Paths in Binary Tree.java
@@ -0,0 +1,28 @@
+/*
+ * Leetcode problem : https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree
+ */
+
+class Solution {
+    int noOfPaths = 0;
+    public int pseudoPalindromicPaths (TreeNode root) {
+        solve(root, 0);
+        return noOfPp;
+    }
+
+    public void solve(TreeNode root, int path) {
+        if(root == null) {
+            return;
+        }
+
+        path = path ^ (1 << root.val);
+        if(root.left == null && root.right == null) {
+            noOfPaths += (path & (path - 1)) == 0 ? 1 : 0;
+        }
+        solve(root.left, path);
+        if(root.right != null) {
+            solve(root.right, path);
+        }
+        path = path ^ (1 << root.val);
+
+    }
+}
